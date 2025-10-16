da558213cead9ccf621df8bf899fbc2075e663a9
Yaseen Halabi <yaseenhalabi@gmail.com>
2025-10-16 13:31:11 -0500

    changed date

--- src/screens/ScreenThree.tsx
+++ src/screens/ScreenThree.tsx
@@ -4,7 +4,7 @@ export default function ScreenThree() {
       <div className="screen-content">
         <h1>Participate</h1>
         <p className="apply-deadline">
-          Applications close Oct 15th, 11:59 PM. For more info, see FAQ.
+          Applications close Oct 25th, 11:59 PM. For more info, see FAQ.
         </p>
         <a
           className="apply-button"
