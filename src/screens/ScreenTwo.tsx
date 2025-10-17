55aea02fd008d97a5347829419d9863d2c39dad4
Yaseen Halabi <yaseenhalabi@gmail.com>
2025-10-16 21:29:47 -0500

    changed wording on about section

--- src/screens/ScreenTwo.tsx
+++ src/screens/ScreenTwo.tsx
@@ -7,13 +7,19 @@ export default function ScreenTwo() {
         <div className="about-text">
           <h1>About</h1>
           <p className="about-paragraph">
-            Join us for MTCHacks, a weekend of collaboration, learning, and
-            creativity dedicated to solving challenges faced by the modern
-            Ummah.
-            <br />
+            MTCHacks is a Muslim-focused hackathon dedicated to solving
+            challenges faced by the modern Ummah. You do not have to be Muslim
+            to participate and/or win!
+            {/* Only render <br />s if not on mobile */}
+            {window.innerWidth > 600 && (
+              <>
+                <br />
+                <br />
+              </>
+            )}
             Compete in one of our three tracks (two technical and one pitch
-            competition) for a chance to win $2000. Whether you come solo or
-            with a team, all are welcome!
+            competition) for a chance to win $2000! Feel free to come solo or
+            with a team, all are welcome.
           </p>
         </div>
         <img src={uiucPic} alt="UIUC campus" className="about-image" />
