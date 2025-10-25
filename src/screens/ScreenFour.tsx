a54d920fa7eec2e6c057278b28921be4945fc279
Yaseen Halabi <yaseenhalabi@gmail.com>
2025-10-25 13:09:13 -0500

    applications closed

--- src/screens/ScreenFour.tsx
+++ src/screens/ScreenFour.tsx
@@ -3,17 +3,15 @@ export default function ScreenFour() {
     <section id="page4" className="screen">
       <div className="screen-content">
         <h1>Participate</h1>
-        <p className="apply-deadline">
-          Applications close Oct 25th, 11:59 PM. For more info, see FAQ.
-        </p>
-        <a
+        <p className="apply-deadline">Applications closed Oct 24th, 11:59 PM</p>
+        {/* <a
           className="apply-button"
           href="https://docs.google.com/forms/d/e/1FAIpQLSeDD_MBduw_t_C4m5BUpC0hCYF4CX4iaL3C1qY_NUJMVdQoXw/viewform"
           target="_blank"
           rel="noopener noreferrer"
         >
           Apply Now
-        </a>
+        </a> */}
       </div>
     </section>
   );
